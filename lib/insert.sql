ba919e94241dbb9e9d4f9499fa530d62a1d59be6
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-07-19 20:07:15 +0300

    fix: Set up bears db and write the necessary queries.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Mr. Chocolate",5,"M","Brown","High",TRUE);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Rowdy",7,"F","White","Extreme",TRUE);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Tabitha",2,"F","White","Low",TRUE);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Sergeant Brown",10,"M","Brown","Medium",TRUE);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Melissa",4,"F","Black","Low",FALSE);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Grinch",12,"M","Green","High",TRUE);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Wendy",7,"F","Black","High",FALSE);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (NULL,1,"F","Black","Extreme",TRUE);
